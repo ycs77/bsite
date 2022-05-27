0991e5c38cd26dabaf7536148f07b180a4403b4b
Lucas Yang <yangchenshin77@gmail.com>
2022-05-27 17:54:05 +0800

    Trun off default is_login

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener(({ reason }) => {
   if (reason === 'install') {
     chrome.storage.local.set({
-      is_login: true,
+      is_login: false,
     })
 
     chrome.storage.sync.set({
